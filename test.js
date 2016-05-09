45b3e793953a21751742a7bbe3821da82356937a
David Lewallen <lewallen.david@gmail.com>
2016-05-08 22:40:48 -0500

    Revert "IPv4 to int32"
    This reverts commit 347e44aafa0354fe2d33ee53a6c126c02aad35a5.

--- test.js
+++ test.js
@@ -1,43 +1,13 @@
-function dec2bin(decimal) {
-  // var binary = "";
-  //
-  // for(var i = bit -1; i >= 0; i++) {
-  //   var binDec = Math.pow(2, i)
-  //
-  //   if(binDec <= decimal) {
-  //     decimal = decimal - binDec;
-  //     binary += "1";
-  //   } else {
-  //     binary += "0";
-  //   }
-  // }
-  // return binary;
+var ddArray = [[1,2,3,4,5],[6,7,8,9,10]];
+var newArray = [];
 
+var test = ddArray.forEach(function(el) {
+	newArray.push(el + 1)
+});
 
-}
+console.log(test);
 
-function ipToInt32(ip){
-  var oneOctet = ip.substring(0,3);
-  var twoOctet = ip.substring(4,7);
-  var threeOctet = ip.substring(8,10);
-  var fourOctet = ip.substring(11);
-
-  //Convert decimal to 8 bit binary
-  //
-  var binary = function(input) {
-    var results = "";
-    for(var i = 7; i >= 0; i--) {
-      if(input <= Math.pow(2, i)) {
-          results +=
-      }
-    }
-    return results
-
-}
-
-function bin2dec(binary) {
-
-  for(var i = binary.length - 1; i > 0; i++) {
-    binary - Math.pow(2, i);
-  }
-}
+var 
+console.log(ddArray.map(function(el) {
+	return el + 1;
+}))
